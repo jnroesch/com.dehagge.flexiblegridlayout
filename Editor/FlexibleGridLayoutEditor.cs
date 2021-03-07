406ecdd6a1d5393429f48d247a3bcc9da23b2110
Jan Niklas Roesch <j.n.roesch@googlemail.com>
2021-03-07 19:50:59 +0100

    upload initial package

---
+++ Editor/FlexibleGridLayoutEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Packages.com.dehagge.flexiblegridlayout.Runtime;
+using UnityEditor;
+
+namespace Packages.com.dehagge.flexiblegridlayout.Editor
+{
+    [CustomEditor(typeof(FlexibleGridLayout))]
+    public class FlexibleGridLayoutEditor : UnityEditor.Editor
+    {
+        private bool _paddingFoldout;
+        
+        public override void OnInspectorGUI()
+        {
+            var flexibleGridLayout = target as FlexibleGridLayout;
+            
+            _paddingFoldout = EditorGUILayout.Foldout(_paddingFoldout, "Padding");
+            if (_paddingFoldout)
+            {
+                var indentLevel = EditorGUI.indentLevel;
+                EditorGUI.indentLevel++;
+                
+                flexibleGridLayout.padding.left = EditorGUILayout.IntField("Left", flexibleGridLayout.padding.left);
+                flexibleGridLayout.padding.right = EditorGUILayout.IntField("Right", flexibleGridLayout.padding.right);
+                flexibleGridLayout.padding.top = EditorGUILayout.IntField("Top", flexibleGridLayout.padding.top);
+                flexibleGridLayout.padding.bottom = EditorGUILayout.IntField("Bottom", flexibleGridLayout.padding.bottom);
+                
+                //reset indent level
+                EditorGUI.indentLevel = indentLevel;
+            }
+
+            flexibleGridLayout.spacing = EditorGUILayout.Vector2Field("Spacing", flexibleGridLayout.spacing);
+
+            flexibleGridLayout.fitType = (FlexibleGridLayout.FitType)EditorGUILayout.EnumPopup("Fit Type", flexibleGridLayout.fitType);
+
+            switch (flexibleGridLayout.fitType)
+            {
+                case FlexibleGridLayout.FitType.FixedColumns:
+                    flexibleGridLayout.columns = EditorGUILayout.IntField("Columns", flexibleGridLayout.columns);
+                    break;
+                case FlexibleGridLayout.FitType.FixedRows:
+                    flexibleGridLayout.rows = EditorGUILayout.IntField("Rows", flexibleGridLayout.rows);
+                    break;
+                case FlexibleGridLayout.FitType.Uniform:
+                    break;
+                case FlexibleGridLayout.FitType.Width:
+                    break;
+                case FlexibleGridLayout.FitType.Height:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            flexibleGridLayout.CalculateLayoutInputVertical();
+        }
+    }
+}
---
+++ Runtime/FlexibleGridLayout.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Packages.com.dehagge.flexiblegridlayout.Runtime
+{
+    public class FlexibleGridLayout : LayoutGroup
+    {
+        public enum FitType
+        {
+            Uniform,
+            Width,
+            Height,
+            FixedRows,
+            FixedColumns
+        }
+
+        public FitType fitType;
+        public Vector2 spacing;
+
+        public int rows;
+        public int columns;
+        
+        private Vector2 cellSize;
+        private bool fitX;
+        private bool fitY;
+
+        public override void CalculateLayoutInputVertical()
+        {
+            base.CalculateLayoutInputHorizontal();
+
+            if(fitType == FitType.Width || fitType == FitType.Height || fitType == FitType.Uniform)
+            {
+                fitX = true;
+                fitY = true;
+                var sqrRt = Mathf.Sqrt(transform.childCount);
+                rows = Mathf.CeilToInt(sqrRt);
+                columns = Mathf.CeilToInt(sqrRt);
+            }
+
+            if(fitType == FitType.Width || fitType == FitType.FixedColumns || fitType == FitType.Uniform)
+            {
+                rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+            }
+            if (fitType == FitType.Height || fitType == FitType.FixedRows || fitType == FitType.Uniform)
+            {
+                columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+            }
+
+            var parentWidth = rectTransform.rect.width;
+            var parentHeight = rectTransform.rect.height;
+
+            var cellWidth = parentWidth / columns - spacing.x / columns * (columns - 1) - padding.left / (float)columns - padding.right / (float)columns;
+            var cellHeight = parentHeight / rows - spacing.y / rows * (rows - 1) - padding.top / (float)rows - padding.bottom / (float)rows;
+
+            cellSize.x = fitX ? cellWidth : cellSize.x;
+            cellSize.y = fitY ? cellHeight : cellSize.y;
+
+            for(var i = 0; i < rectChildren.Count; i ++)
+            {
+                var rowCount = i / columns;
+                var columnCount = i % columns;
+
+                var item = rectChildren[i];
+
+                var xPos = cellSize.x * columnCount + spacing.x * columnCount + padding.left;
+                var yPos = cellSize.y * rowCount + spacing.y * rowCount + padding.top;
+
+                SetChildAlongAxis(item, 0, xPos, cellSize.x);
+                SetChildAlongAxis(item, 1, yPos, cellSize.y);
+            }
+        }
+
+        public override void SetLayoutHorizontal()
+        {
+            //NOP
+        }
+
+        public override void SetLayoutVertical()
+        {
+            //NOP
+        }
+    }
+}
